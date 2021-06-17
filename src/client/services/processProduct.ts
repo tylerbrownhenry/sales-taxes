65e900dc94835455ed838a4df3f8711e60f80b27
Tyler Henry <tylerbrownhenry@yahoo.com>
2021-06-17 16:48:32 -0400

    Cleaning up comments

--- src/client/services/processProduct.ts
+++ src/client/services/processProduct.ts
@@ -2,21 +2,36 @@ import Product from "../types/Product";
 
 export const findTax = (price: number, percent: number) => {
   // Because we all love how JavaScript handles numbers
+  
   // In reality I doubt I would leave so many comments here
-  const rawTax: number = (price / 100) * percent; // Find the raw tax amount based on %
-  let fixedNumber: number = Number(rawTax.toFixed(2)); // Convert it to a number with 2 decimals points
-  let cents: number = fixedNumber % 1; // Find the change
-  fixedNumber -= cents; // Remove the change from the total
-  cents = Number((cents * 100).toFixed(0)); // Convert change to a whole number
-  let singleDigit = cents % 10; // Find the ones digit from the change
-  let remainder = 0; // Assume wont be adding anything if the number is 0
+
+  // Find the raw tax amount based on %
+  // Convert it to a number with 2 decimals points
+  // Find the change
+  // Remove the change from the total
+  // Convert change to a whole number
+  // Find the ones digit from the change
+  // Assume wont be adding anything if the number is 0
+  // If greater than zero round to nearest 5 or 10
+  // Remove the original ones digit
+  // Add 0, 5 or 10
+  // Find the total tax amount
+  // Add the total tax amount to the price
+  
+  const rawTax: number = (price / 100) * percent; 
+  let fixedNumber: number = Number(rawTax.toFixed(2)); 
+  let cents: number = fixedNumber % 1; 
+  fixedNumber -= cents; 
+  cents = Number((cents * 100).toFixed(0)); 
+  let singleDigit = cents % 10; 
+  let remainder = 0; 
   if (singleDigit !== 0) {
-    remainder = singleDigit < 5 ? 5 : 10; // If greater than zero round to nearest 5 or 10
+    remainder = singleDigit < 5 ? 5 : 10; 
   }
-  cents -= singleDigit; // Remove the original ones digit
-  cents += remainder; // Add 0, 5 or 10
-  const tax = fixedNumber + cents / 100; // Find the total tax amount
-  const taxedPrice = Number((price + tax).toFixed(2)); // Add the total tax amount to the price
+  cents -= singleDigit; 
+  cents += remainder; 
+  const tax = fixedNumber + cents / 100; 
+  const taxedPrice = Number((price + tax).toFixed(2)); 
   return {
     taxedPrice,
     tax,
