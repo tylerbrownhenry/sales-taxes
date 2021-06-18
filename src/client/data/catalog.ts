cf31587376b1711820b57f2a1fbbaf656eeadb9a
Tyler Henry <tylerbrownhenry@yahoo.com>
2021-06-18 03:20:29 -0400

    Running Prettier

--- src/client/data/catalog.ts
+++ src/client/data/catalog.ts
@@ -1,84 +1,84 @@
 // Api Data
 const data = [
-  {
-    name: "Chocolate Bar",
-    type: "food",
-    imported: false,
-    price: 0.85,
-  },
-  {
-    name: "Imported Box of Chocolates",
-    type: "food",
-    imported: true,
-    price: 10.0,
-  },
-  {
-    name: "Headache Medicine",
-    type: "medical",
-    imported: true,
-    price: 9.75,
-  },
-  {
-    name: "Imported Bottle of Russin Perfume",
-    type: "perfume",
-    imported: true,
-    price: 47.5,
-  },
-  {
-    name: "Imported Bottle of French Perfume",
-    type: "perfume",
-    imported: true,
-    price: 27.99,
-  },
-  {
-    name: "Imported box of spanish chocolates",
-    type: "food",
-    imported: true,
-    price: 11.25,
-  },
-  {
-    name: "Doritos",
-    type: "food",
-    imported: false,
-    price: 0.85,
-  },
-  {
-    name: "Dante's Inferno",
-    type: "book",
-    imported: false,
-    price: 12.49,
-  },
-  {
-    name: "Of Mice and Men",
-    type: "book",
-    imported: false,
-    price: 12.49,
-  },
+    {
+        name: 'Chocolate Bar',
+        type: 'food',
+        imported: false,
+        price: 0.85,
+    },
+    {
+        name: 'Imported Box of Chocolates',
+        type: 'food',
+        imported: true,
+        price: 10.0,
+    },
+    {
+        name: 'Headache Medicine',
+        type: 'medical',
+        imported: true,
+        price: 9.75,
+    },
+    {
+        name: 'Imported Bottle of Russin Perfume',
+        type: 'perfume',
+        imported: true,
+        price: 47.5,
+    },
+    {
+        name: 'Imported Bottle of French Perfume',
+        type: 'perfume',
+        imported: true,
+        price: 27.99,
+    },
+    {
+        name: 'Imported box of spanish chocolates',
+        type: 'food',
+        imported: true,
+        price: 11.25,
+    },
+    {
+        name: 'Doritos',
+        type: 'food',
+        imported: false,
+        price: 0.85,
+    },
+    {
+        name: "Dante's Inferno",
+        type: 'book',
+        imported: false,
+        price: 12.49,
+    },
+    {
+        name: 'Of Mice and Men',
+        type: 'book',
+        imported: false,
+        price: 12.49,
+    },
 
-  {
-    name: "The Very Best of GWAR Ultimate Edition",
-    type: "music",
-    imported: false,
-    price: 12.49,
-  },
-  {
-    name: "The King's Speech (Unabriged Version",
-    type: "book",
-    imported: false,
-    price: 30.89,
-  },
-  {
-    name: "The White Album",
-    type: "music",
-    imported: false,
-    price: 10.48,
-  },
-  {
-    name: "The Black Album",
-    type: "music",
-    imported: false,
-    price: 10.49,
-  },
+    {
+        name: 'The Very Best of GWAR Ultimate Edition',
+        type: 'music',
+        imported: false,
+        price: 12.49,
+    },
+    {
+        name: "The King's Speech (Unabriged Version",
+        type: 'book',
+        imported: false,
+        price: 30.89,
+    },
+    {
+        name: 'The White Album',
+        type: 'music',
+        imported: false,
+        price: 10.48,
+    },
+    {
+        name: 'The Black Album',
+        type: 'music',
+        imported: false,
+        price: 10.49,
+    },
 ];
 
 export default data;
--- src/client/services/processProduct.ts
+++ src/client/services/processProduct.ts
@@ -1,82 +1,79 @@
-import Product from "../types/Product";
+import Product from '../types/Product';
 
 export const findTax = (price: number, percent: number) => {
-  // Because we all love how JavaScript handles numbers
-  
-  // In reality I doubt I would leave so many comments here
+    // Because we all love how JavaScript handles numbers
 
-  // Find the raw tax amount based on %
-  // Convert it to a number with 2 decimals points
-  // Find the change
-  // Remove the change from the total
-  // Convert change to a whole number
-  // Find the ones digit from the change
-  // Assume wont be adding anything if the number is 0
-  // If greater than zero round to nearest 5 or 10
-  // Remove the original ones digit
-  // Add 0, 5 or 10
-  // Find the total tax amount
-  // Add the total tax amount to the price
-  
-  const rawTax: number = (price / 100) * percent; 
-  let fixedNumber: number = Number(rawTax.toFixed(2)); 
-  let cents: number = fixedNumber % 1; 
-  fixedNumber -= cents; 
-  cents = Number((cents * 100).toFixed(0)); 
-  let singleDigit = cents % 10; 
-  let remainder = 0; 
-  if (singleDigit !== 0) {
-    remainder = singleDigit < 5 ? 5 : 10; 
-  }
-  cents -= singleDigit; 
-  cents += remainder; 
-  const tax = fixedNumber + cents / 100; 
-  const taxedPrice = Number((price + tax).toFixed(2)); 
-  return {
-    taxedPrice,
-    tax,
-  };
+    // In reality I doubt I would leave so many comments here
+
+    // Find the raw tax amount based on %
+    // Convert it to a number with 2 decimals points
+    // Find the change
+    // Remove the change from the total
+    // Convert change to a whole number
+    // Find the ones digit from the change
+    // Assume wont be adding anything if the number is 0
+    // If greater than zero round to nearest 5 or 10
+    // Remove the original ones digit
+    // Add 0, 5 or 10
+    // Find the total tax amount
+    // Add the total tax amount to the price
+
+    const rawTax: number = (price / 100) * percent;
+    let fixedNumber: number = Number(rawTax.toFixed(2));
+    let cents: number = fixedNumber % 1;
+    fixedNumber -= cents;
+    cents = Number((cents * 100).toFixed(0));
+    let singleDigit = cents % 10;
+    let remainder = 0;
+    if (singleDigit !== 0) {
+        remainder = singleDigit < 5 ? 5 : 10;
+    }
+    cents -= singleDigit;
+    cents += remainder;
+    const tax = fixedNumber + cents / 100;
+    const taxedPrice = Number((price + tax).toFixed(2));
+    return {
+        taxedPrice,
+        tax,
+    };
 };
 
 export const checkIfSalesTaxExempt = (type: string) => {
-  // Check if item is in a sales tax exempt categrory
-  return type === "food" || type === "medical" || type == "book";
+    // Check if item is in a sales tax exempt categrory
+    return type === 'food' || type === 'medical' || type == 'book';
 };
 
 export const processProducts = (data: any) => {
-  // Preprocess the tax values and add ids to the catalog data
-  // In real world wouldn't do it like this
-  data.reduce(
-    (accumulator: Product[], product: Product, currentIndex: number) => {
-      let newProduct = product;
-      const { imported, price, type } = newProduct;
-      const taxExempt = checkIfSalesTaxExempt(type);
-      let taxRate = 0;
+    // Preprocess the tax values and add ids to the catalog data
+    // In real world wouldn't do it like this
+    data.reduce((accumulator: Product[], product: Product, currentIndex: number) => {
+        let newProduct = product;
+        const { imported, price, type } = newProduct;
+        const taxExempt = checkIfSalesTaxExempt(type);
+        let taxRate = 0;
 
-      if (!taxExempt) {
-        taxRate += 10;
-      }
+        if (!taxExempt) {
+            taxRate += 10;
+        }
 
-      if (imported === true) {
-        taxRate += 5;
-      }
+        if (imported === true) {
+            taxRate += 5;
+        }
 
-      const { taxedPrice, tax } = findTax(price, taxRate);
+        const { taxedPrice, tax } = findTax(price, taxRate);
 
-      newProduct.taxExempt = taxExempt;
-      newProduct.taxRate = taxRate;
-      newProduct.taxedPrice = taxedPrice;
-      newProduct.tax = tax;
-      newProduct.quantity = 0;
-      newProduct.id = `product_id_${currentIndex}`;
+        newProduct.taxExempt = taxExempt;
+        newProduct.taxRate = taxRate;
+        newProduct.taxedPrice = taxedPrice;
+        newProduct.tax = tax;
+        newProduct.quantity = 0;
+        newProduct.id = `product_id_${currentIndex}`;
 
-      accumulator.push(newProduct);
-      return accumulator;
-    },
-    []
-  );
+        accumulator.push(newProduct);
+        return accumulator;
+    }, []);
 
-  return data;
+    return data;
 };
 
 export default processProducts;
